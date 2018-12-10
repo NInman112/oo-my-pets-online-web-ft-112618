75d51117a12650dc3223c4ba52f0cae3c336f277
Nick Inman <nick@theoatmeal.com>
2018-12-10 00:52:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
+require"pry"
+
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.count
+    
+  end 
+
+  
+  def say_species
+    "I am a #{species}."
+  end 
+  
+  def name(name)
+    @name = name 
+  end 
+  
+  def pets
+    pets = {:fishes => [], :dogs => [], :cats => []}
+  end 
+
 end
